126bf3ddf2526a3294cc59b8f8259b3f6a2f6848
Tom Fang <tianhuaf0526@gmail.com>
2021-08-25 17:42:18 -0400

    Add WakeOnLan

---
+++ WakeOnLan/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WakeOnLan.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            
+        }
+
+        private Configuration config;
+         public Configuration WoLConfig
+        {
+            get { return config; }
+            set
+            {
+                config = value;
+                OnPropertyChanged(nameof(WoLConfig));
+            }
+        }
+
+
+        // Bind Record
+        private Dictionary<string, ComputerInfo> records;
+        public Dictionary<string, ComputerInfo> Records
+        {
+            get { return records; }
+            set
+            {
+                records = value;
+                OnPropertyChanged(nameof(Records));
+            }
+        }
+    }
+}
---
+++ WakeOnLan/ViewModel/ViewModelBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+
+
+namespace WakeOnLan.ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            this.OnDispose();
+        }
+
+        protected virtual void OnDispose()
+        {
+        }
+
+        #endregion
+
+    }
+}
+
